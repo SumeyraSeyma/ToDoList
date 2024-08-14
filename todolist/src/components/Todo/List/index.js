c9f91d5d678311c5658279a302b6e2f5956e9720
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-08-14 12:12:51 +0300

    checkbox

--- todolist/src/components/Todo/List/index.js
+++ todolist/src/components/Todo/List/index.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function List({todos}) {
+    const [checkedItems, setCheckedItems] = useState(new Array(todos.length).fill(false));
+
+  const onClickCheckbox = (index) => {
+    const updatedCheckedItems = [...checkedItems];
+    updatedCheckedItems[index] = !updatedCheckedItems[index];
+    setCheckedItems(updatedCheckedItems);
+    };
+
+
   return (
     <div>
         <ul>
 
             {
                 todos.map((todos,i)=>
-                    <li key={i}>
+                    <li key={i} style={{ textDecoration: checkedItems[i] ? 'line-through' : 'none' }} >
                         {todos.title}
-                    </li>
-                )
+                        <input
+                        checked={checkedItems[i]}
+                        onChange={() => onClickCheckbox(i)}
+                        type='checkbox'
+                        />
+                    </li >)
             }
 
+
+            
+
         </ul>
+
+        <p>
+        {todos.length} items left
+      </p>
+
     </div>
   )
 }
--- todolist/src/components/Todo/index.js
+++ todolist/src/components/Todo/index.js
@@ -6,15 +6,15 @@ function Todo() {
     const [todos, setTodos] = React.useState([{
         id: 1,
         title: 'Learn React',
-        completed: false
+ 
     }, {
         id: 2,
         title: 'Learn Vue',
-        completed: false
+
     }, {
         id: 3,
         title: 'Learn Angular',
-        completed: false
+ 
     }])
 
     useEffect(() => {
